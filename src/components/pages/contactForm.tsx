00f57362367aa087cc8277e336865bd37f00a3b6
Bharat Raj Verma <bharat.raj1508@gmail.com>
2024-10-30 17:14:23 +0530

    added link to github and linkdedIn

--- src/components/pages/contactForm.tsx
+++ src/components/pages/contactForm.tsx
@@ -62,27 +62,31 @@ export function contactForm() {
           <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
 
           <div className="flex flex-col space-y-4">
-            <button
+            <a
               className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
               type="submit"
+              href="https://github.com/bharatraj1508"
+              target="_blank"
             >
               <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
               <span className="text-neutral-700 dark:text-neutral-300 text-sm">
                 GitHub
               </span>
               <BottomGradient />
-            </button>
+            </a>
 
-            <button
+            <a
               className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
               type="submit"
+              href="https://www.linkedin.com/in/bharat-raj-verma/"
+              target="_blank"
             >
               <IconBrandLinkedin className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
               <span className="text-neutral-700 dark:text-neutral-300 text-sm">
                 LinkedIn
               </span>
               <BottomGradient />
-            </button>
+            </a>
           </div>
         </form>
       </div>
