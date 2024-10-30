65fcc9fd8a577e4ca3fcb3c8c52ff4fcaac6dc45
Bharat Raj Verma <bharat.raj1508@gmail.com>
2024-10-30 10:30:45 +0530

    completed the landing page

--- src/components/pages/landingPage.tsx
+++ src/components/pages/landingPage.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import { TypewriterEffect } from "../ui/typewriter-effect";
 import IntroPic from "../../assets/intro/heroImage.png";
-import BgPic from "../../assets/intro/bg.png";
+import { motion } from "framer-motion";
+import logo from "../../../public/logo.png";
+
+import { AuroraBackground } from "../ui/aurora-background";
 
 const landingPage = () => {
   const words = [
@@ -24,30 +29,41 @@ const landingPage = () => {
   ];
   return (
     <>
-      <div
-        className="relative flex items-center justify-center h-screen bg-cover bg-center opacity-90"
-        style={{ backgroundImage: `url(${BgPic})` }}
-      >
-        {/* Overlay for opacity */}
-        <div className="absolute inset-0 bg-white opacity-40"></div>
-
-        <div className="relative z-10 flex flex-row items-center h-screen justify-between w-full">
-          <div className="flex flex-col items-center w-[100rem]">
-            <p className="text-neutral-600 dark:text-neutral-200 text-base mb-10">
-              Crafting code with passion and purpose
-            </p>
-            <TypewriterEffect words={words} />
+      <AuroraBackground>
+        <motion.div
+          initial={{ opacity: 0.0, y: 40 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="relative w-full h-full"
+        >
+          <div className="absolute p-8">
+            <img src={logo} alt="" className="w-[3rem]" />
           </div>
 
-          <div>
-            <img
-              src={IntroPic}
-              alt="my profile"
-              className="rounded-full mx-auto w-3/4 shadow-2xl"
-            />
+          <div className="h-full w-full dark:bg-black dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
+            <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black  [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+            <div className="relative z-10 flex flex-row items-center h-screen w-full space-x-6 px-8">
+              <div className="flex flex-col items-center w-[100rem]">
+                <p className="text-neutral-600 dark:text-neutral-200 text-base mb-10">
+                  Crafting code with passion and purpose
+                </p>
+                <TypewriterEffect words={words} />
+              </div>
+              <div className=" hidden md:block bg-neutral-100 w-2/3 rounded-full transition-all duration-500 hover:shadow-2xl hover:shadow-blue-500/50 hover:scale-100">
+                <img
+                  src={IntroPic}
+                  alt="my profile"
+                  className="rounded-full shadow-lg mx-auto w-full hover:scale-110 transition-transform duration-300 ease-in-out"
+                />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </motion.div>
+      </AuroraBackground>
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,8 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+const svgToDataUri = require("mini-svg-data-uri");
+
+const colors = require("tailwindcss/colors");
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
@@ -14,6 +19,12 @@ module.exports = {
   darkMode: "class",
   prefix: "",
   theme: {
+    extend: {
+      animation: {
+        aurora: "aurora 60s linear infinite",
+      },
+    },
+
     container: {
       center: true,
       padding: "2rem",
@@ -71,14 +82,48 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        aurora: {
+          from: {
+            backgroundPosition: "50% 50%, 50% 50%",
+          },
+          to: {
+            backgroundPosition: "350% 50%, 350% 50%",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        aurora: "aurora 60s linear infinite",
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), addVariablesForColors],
+  plugins: [
+    require("tailwindcss-animate"),
+    addVariablesForColors,
+    function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "bg-grid": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-grid-small": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-dot": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
 };
 
 // This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
